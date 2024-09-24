d5f2f41dcf2ee560b4fc54706022e44d5e34763e
Leonardo Lima <leonardo403@gmail.com>
2024-09-24 00:21:12 -0300

    create module and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [RouterOutlet, MatToolbarModule]
 })
 export class AppComponent {
   title = 'Projeto Mission Home';
